55083d393fd4a4be5a310152e20c81f55de905af
oTuff <oscartuff@protonmail.com>
2023-01-11 14:11:12 +0100

    initial commit

---
+++ src/main/java/entities/Role.java
@@ -0,0 +1,65 @@
+package entities;
+
+import dtos.UserDTO;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Plaul
+ */
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "role_name", length = 20)
+    private String roleName;
+    
+    @ManyToMany(mappedBy = "roleList", cascade = CascadeType.PERSIST)
+    private List<User> userList;
+
+    public Role() {
+    }
+
+    public Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+        Role role = (Role) o;
+        return getRoleName().equals(role.getRoleName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRoleName());
+    }
+}
